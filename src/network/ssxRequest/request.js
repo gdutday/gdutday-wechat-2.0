9954740eddaf6eecb5fe11d0e1fb91c11467d193
zhangmingyuan666 <2369558390@qq.com>
2024-01-30 20:02:48 +0800

    fix: 部分mock代码

--- src/network/ssxRequest/request.js
+++ src/network/ssxRequest/request.js
@@ -237,3 +237,122 @@ export function requestSsxGraduate(config) {
 
   return requestsTool(config)
 }
+
+export function requestSsxV3(config) {
+  //config用于接收配置文件
+  const instanceSsx = axios.create({
+    baseURL: 'https://gdutdays.gdutelc.com/v3',
+    //baseURL:'http://192.168.123.44:8848/gdutday2',
+    // baseURL:'http://192.168.123.148:8080/usedPlatform/common/test'
+    timeout: 5000,
+  })
+
+  //请求拦截
+  instanceSsx.interceptors.request.use(
+    config => {
+      //如果有一个接口需要认证才能访问，就在这里统一进行设置
+      // 比如token
+
+      return config
+      //直接放行
+    },
+    err => {
+      console.log(err)
+    }
+  )
+
+  // 响应拦截
+
+  instanceSsx.interceptors.response.use(
+    res => {
+      console.log(res)
+      console.log('responsetrue')
+      //这个用于判断网络是否错误
+      if (!res.status) {
+        uni.showToast({
+          title: '网络错误哟~',
+          duration: 2000,
+          icon: 'error',
+        })
+        throw new Error('网络错误哟~')
+      }
+
+      // if(!res.status){
+      //   uni.showToast({
+      //     title: '网络错误哟~',
+      //     duration: 2000,
+      //     icon: "error",
+      //   });
+      //   throw new Error('网络错误哟~');
+      // }
+      //以下用于获取后台返回的信息
+      let info = res.data.msg ? res.data.msg : ''
+      //非4000时的数值
+      if (res.data.code != 4000) {
+        switch (res.data.code) {
+          case 4001:
+            uni.showToast({
+              title: '请先前往登录熬',
+              duration: 2000,
+              icon: 'error',
+            })
+            break
+          case 4002:
+            // uni.showToast({
+            //   title: info,
+            //   duration: 2000,
+            //   icon: "error",
+            // });
+            break
+          case 4003:
+            // uni.showToast({
+            //   title: info,
+            //   duration: 2000,
+            //   icon: "error",
+            // });
+            break
+          case 4004: {
+            uni.showToast({
+              title: '后台正在维护中',
+              duration: 2000,
+              icon: 'error',
+            })
+            break
+          }
+          default: {
+            info = '网络错误'
+            uni.showToast({
+              title: info,
+              duration: 2000,
+              icon: 'error',
+            })
+            break
+          }
+        }
+        throw new Error(info)
+      }
+
+      return res.data ? res.data : res
+    },
+    err => {
+      console.log('responseerror')
+      uni.showToast({
+        title: '网络错误哟~',
+        duration: 2000,
+        icon: 'error',
+      })
+      throw new Error('网络错误哟~')
+      //  if(err.response.data.code == '4001')
+      //  // 当状态码为未登陆的时候
+      //  {
+      //       uni.navigateTo({
+      //          url: 'pages/profile/Login'
+      //       });
+      //  }
+      //如果是需要授权才可与访问的接口同意去login授权
+      //如果有错误这里会
+    }
+  )
+
+  return instanceSsx(config)
+}
---
+++ src/pages/login-v2/index.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <button @click="login1">点我登陆本科生 教务系统</button>
+        <button @click="login2">点我登陆本科生 统一系统</button>
+        <button @click="login3">点我登陆研究生 统一认证系统</button>
+    </div>
+</template>
+
+<script>
+import {requestSsxV3} from '@/network/ssxRequest/request.js'
+import {ref} from 'vue'
+// 研究生部分
+import {
+	getScheduleGraduateInfo,
+	getScheduleGraduateInfoByCookies,
+	getScoreGraduate,
+	stuLoginGraduate,
+	checkCookies,
+	getGraduteUserInfo,
+	checkCaptcha,
+	saveRefreshTime
+} from '@/network/ssxRequest/ssxInfo/graduateAllInfo.js'
+
+import {graduteEncoding,
+        logOutInit,
+		getStorageSync,
+		filterSchedule,
+		handleSchedule,
+} from '@/utils/common.js'
+
+export default {
+    setup() {
+        const userName2 = ref('3122002311')
+        const password2 = ref('UA8LlkfehaBKZ3iyMzIGNQ==')
+
+        const userName3 = ref('2112205176')
+        const password3 = ref('OcJ2DWU3hBnjdymzqMYQXg==')
+
+        //点我登陆本科生 教务系统
+        const login1 = () => {
+
+        }
+
+        // 点我登陆本科生 统一系统
+        const login2 = () => {
+            let params = {
+					user: userName2.value,
+					password:  password2.value,
+                    "userType":1,
+                    "loginType":2,
+				}
+
+            requestSsxV3({
+                url: '/login',
+                method: 'POST',
+                data: params,
+                // params:params,
+                // 接口使用json
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+        }
+
+        //点我登陆研究生 统一认证系统
+        const login3 = () => {
+            let params = {
+					user: userName3.value,
+					password:  password3.value,
+                    "userType":2    ,
+                    "loginType":2,
+				}
+
+            requestSsxV3({
+                url: '/login',
+                method: 'POST',
+                data: params,
+                // params:params,
+                // 接口使用json
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+        }
+
+
+        return {
+            login1,
+            login2,
+            login3
+        }
+    }
+}
+</script>
