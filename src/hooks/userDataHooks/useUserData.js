c5edb7c503a83ed5b8d60b187775773e7551fa2d
zhangmingyuan666 <2369558390@qq.com>
2024-02-01 21:38:59 +0800

    fix: 增加mock代码

--- src/hooks/userDataHooks/useUserData.js
+++ src/hooks/userDataHooks/useUserData.js
@@ -1,7 +1,9 @@
 import { ref } from 'vue';
 import useToast from '../toastHooks/useToast';
 import useLogin from '../loginHooks/useLogin';
-
+import {loginV2, sendVerV2}from '@/network/ssxRequest/request-v2/login.js'
+import {getScoreV2} from '@/network/ssxRequest/request-v2/score.js'
+import { getExaminationV2 } from '@/network/ssxRequest/request-v2/examination';
 
 export default function () {
     const {
@@ -21,7 +23,13 @@ export default function () {
     } = useLogin()
 
     const preCommonRequestHandler = (params, combineLoginType) => {
+        let tmp = {
+            ...params,
+            userType,
+            loginType
+        }
 
+        return tmp
     }
 
     
@@ -45,19 +53,17 @@ export default function () {
 
     // 根据userLoginType决定到底采用哪个函数(在外部进行组合)
     const getSchedule = commonRequest(async (params) => {
+
         switch (combineLoginType) {
             case LOGIN_ENUM.UG_V1: {
-                const result = await req
-
+                const result = await loginV2()
                 // 做一层处理
             }
             case LOGIN_ENUM.PG_V2:
             case LOGIN_ENUM.UG_V2: {
-                params = {
-                    ...params,
-                    userType,
-                    loginType
-                }
+                
+                const result = await loginV2()
+
                 break
             }
         }
@@ -65,16 +71,23 @@ export default function () {
 
     //
     const getExam = commonRequest(async (params) => {
-        sw
+        const result = await getExaminationV2()
     })
 
     const getGrade = commonRequest(async (params) => {
-        
+        const result = await getScoreV2()
     })
+
+    const getVerV2 = async () => {
+        const result = await sendVerV2()
+    }
+
+
 
     return {
         getSchedule,
         getExam,
-        getGrade
+        getGrade,
+        getVerV2
     }
 }
--- src/pages/login-v2/index.vue
+++ src/pages/login-v2/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <div style="margin-top: 30px">
+        <button @click="setCombineTest(1, 1)">
+            设置本科生 - 教务系统
+        </button>
+        <button @click="setCombineTest(2, 2)">
+            设置研究生 - 统一验证
+        </button>
+        <button @click="setCombineTest(1, 2)">
+            设置本科生 - 统一验证
+        </button>
       <span>loginType: </span>
       <input type="text" v-model="loginType" @input="handleInputLoginType" />
     </div>
@@ -12,6 +21,13 @@
     <div>分割线</div>
 
     <button @click="testLogin">点我登陆</button>
+
+    <div>分割线</div>
+
+    <button @click="getVerV2">点我获取验证码</button>
+    <button @click="getSchedule">点我获取课表</button>
+    <button @click="getExam">点我获取成绩</button>
+    <button @click="getGrade">点我获取考试</button>
   </div>
 </template>
 
@@ -20,6 +36,7 @@ import { requestSsxV3 } from "@/network/ssxRequest/request.js";
 import { ref } from "vue";
 import * as LOGIN_ENUM from '@/modules/login/enum';
 import useLogin from "@/hooks/loginHooks/useLogin.js";
+import useUserData from '@/hooks/userDataHooks/useUserData.js'
 // 研究生部分
 import {
   graduteEncoding,
@@ -32,8 +49,23 @@ import {
 
 export default {
   setup() {
-    const loginType = ref(1);
-    const userType = ref(1);
+    const {
+      login,
+      getCurrentLoginType,
+      getCurrentUserType,
+      combineLoginTypeAction,
+      separateLoginTypeAction,
+    } = useLogin();
+
+    const {
+        getSchedule,
+        getExam,
+        getGrade,
+        getVerV2
+    } = useUserData()
+
+    const loginType = ref(getStorageSync('loginType'));
+    const userType = ref(getStorageSync('userType'));
 
     const handleInputLoginType = (e) => {
       loginType.value = Number(e.target.value);
@@ -45,14 +77,12 @@ export default {
       uni.setStorageSync("userType", userType.value);
     };
 
-    const {
-      login,
-      getCurrentLoginType,
-      getCurrentUserType,
-      combineLoginTypeAction,
-      separateLoginTypeAction,
-    } = useLogin();
-
+    const setCombineTest = (userType, loginType) => {
+        uni.setStorageSync("userType", userType);
+        uni.setStorageSync("loginType", loginType);
+    }
+    
+    const testLogin = () => {}
 
 
     return {
@@ -60,7 +90,12 @@ export default {
       userType,
       handleInputLoginType,
       handleInputUserType,
-      testLogin
+      testLogin,
+      setCombineTest,
+      getSchedule,
+        getExam,
+        getGrade,
+        getVerV2
     };
   },
 };
