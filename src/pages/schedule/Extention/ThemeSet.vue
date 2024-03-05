94d1b6516fc526fb2f6398f6ded8238ab47bc81e
zhangmingyuan <2369558390@qq.com>
2024-03-05 23:30:01 +0800

    fix: 修复上传照片bridgeMethod过期问题

--- src/pages/schedule/Extention/ThemeSet.vue
+++ src/pages/schedule/Extention/ThemeSet.vue
@@ -13,43 +13,25 @@
         </template>
         <template v-slot:default>
           <view class="w-1">
-            <view
-              class="ts-desc w-1 px-2 my-2"
-              :style="{ borderBottom: `${getThemeColor.curBg} 2px solid` }"
-              :class="isChange ? 'animation-fade' : ''"
-            >
+            <view class="ts-desc w-1 px-2 my-2" :style="{borderBottom: `${getThemeColor.curBg} 2px solid`}"
+              :class="isChange ? 'animation-fade' : ''">
               <view><text class="iconfont icon-icon-test4 pr-1"></text> 当前主题</view>
-              <view :style="{ color: getThemeColor.curBgSecond }">{{ themeName }}</view>
+              <view :style="{color: getThemeColor.curBgSecond}">{{ themeName }}</view>
             </view>
             <view class="ts-theme w-1 my-2">
-              <view
-                v-for="(value, key) in color"
-                :key="key"
-                @click="setTheme(key, value)"
-                class="ts-theme-item flex-center"
-              >
-                <view
-                  class="ts-theme-item-show"
-                  :style="{
-                    backgroundImage: `linear-gradient(90deg, ${value.bgColor} ,${'#ccc'})`,
-                    marginTop: '20rpx',
-                  }"
-                >
-                  <text
-                    class="t iconfont icon-icon-test45"
-                    :class="isChange ? 'animation-fade' : ''"
-                    v-if="value.bgColor == getThemeColor.curBg"
-                  ></text>
+              <view v-for="(value, key) in color" :key="key" @click="setTheme(key, value)"
+                class="ts-theme-item flex-center">
+                <view class="ts-theme-item-show" :style="{
+                  backgroundImage: `linear-gradient(90deg, ${value.bgColor} ,${'#ccc'})`,
+                  marginTop: '20rpx',
+                }">
+                  <text class="t iconfont icon-icon-test45" :class="isChange ? 'animation-fade' : ''"
+                    v-if="value.bgColor == getThemeColor.curBg"></text>
                 </view>
               </view>
             </view>
-            <ming-list-item
-              content="是否要上传背景图片"
-              correctContent="上传"
-              @cancelMethod="removeBackgroundImage"
-              @correctMethod="openModal"
-              :themeColor="getThemeColor"
-            >
+            <ming-list-item content="是否要上传背景图片" correctContent="上传" @cancelMethod="removeBackgroundImage"
+              @correctMethod="openModal" :themeColor="getThemeColor">
             </ming-list-item>
             <!-- <ming-list-item
               content="是否要反转背景部分字体颜色"
@@ -57,52 +39,34 @@
             >
             </ming-list-item> -->
             <ming-list-item content="透明度设置" :themeColor="getThemeColor">
-              <slider
-                class="p-0 m-0"
-                :style="{ width: '120px' }"
-                min="0"
-                max="100"
-                :value="getOpacity"
-                step="1"
-                :activeColor="getThemeColor.curBgSecond"
-                :block-color="getThemeColor.curBgSecond"
-                @change="sliderChange"
-                show-value
-              />
+              <slider class="p-0 m-0" :style="{width: '120px'}" min="0" max="100" :value="getOpacity" step="1"
+                :activeColor="getThemeColor.curBgSecond" :block-color="getThemeColor.curBgSecond" @change="sliderChange"
+                show-value />
             </ming-list-item>
           </view>
         </template>
       </ming-container>
     </view>
-    <ming-confirm
-      :showedScheduleInfo="showedScheduleInfo"
-      :themeColor="getThemeColor"
-      title="背景图片上传提醒"
+    <ming-confirm :showedScheduleInfo="showedScheduleInfo" :themeColor="getThemeColor" title="背景图片上传提醒"
       content="如果原本有背景图片，那么就会被覆盖。如果没有图片，建议将上传的图片先切割好（尽量以16：9的比例），以后更新时会增加背景切割功能。"
-      @fatherMethod="choosePgPic"
-    ></ming-confirm>
-    <ming-toast
-      :isShow="toastIsShow"
-      @resumeToastIsShow="resumeToastIsShow"
-      :content="warningInfo"
-      :toastType="toastType"
-      :themeColor="getThemeColor"
-    ></ming-toast>
+      @fatherMethod="choosePgPic"></ming-confirm>
+    <ming-toast :isShow="toastIsShow" @resumeToastIsShow="resumeToastIsShow" :content="warningInfo" :toastType="toastType"
+      :themeColor="getThemeColor"></ming-toast>
   </view>
 </template>
 
 <script>
-import { computed, ref, onMounted } from 'vue'
-import { useStore } from 'vuex'
+import {computed, ref, onMounted} from 'vue'
+import {useStore} from 'vuex'
 import Ztl from '@/components/common/Ztl.vue'
 import WatchButton from '@/components/common/WatchButton.vue'
 import MingContainer from '@/components/common/MingContainer'
 import MingListItem from '@/components/common/MingListItem'
 import MingConfirm from '@/components/common/MingConfirm'
 import MingToast from '@/components/common/MingToast'
-import { color } from '@/static/color/color.js'
-import { setThemeColor, getStorageSync, becomePromise } from '@/utils/common.js'
-import { useToast, useMingModal } from '@/hooks/index.js'
+import {color} from '@/static/color/color.js'
+import {setThemeColor, getStorageSync, becomePromise} from '@/utils/common.js'
+import {useToast, useMingModal} from '@/hooks/index.js'
 export default {
   components: {
     Ztl,
@@ -118,9 +82,9 @@ export default {
     let isChange = ref(false)
     let themeName = ref('')
 
-    const { toastType, toastIsShow, resumeToastIsShow, inspireToastIsShow } = useToast()
+    const {toastType, toastIsShow, resumeToastIsShow, inspireToastIsShow} = useToast()
 
-    const { openModal, close } = useMingModal()
+    const {openModal, close} = useMingModal()
 
     const getOpacity = computed(() => store.state.theme.opacity * 100)
 
@@ -153,11 +117,14 @@ export default {
       try {
         const {
           tempFilePaths: [path],
-        } = await becomePromise(uni.chooseImage, { count: 1 }, 'chooseImage')
-        const { savedFilePath: newPath } = await becomePromise(uni.saveFile, { tempFilePath: path }, 'saveFile')
+        } = await becomePromise(uni.chooseImage, {count: 1}, 'chooseImage')
+        const {savedFilePath: newPath} = await becomePromise(wx.getFileSystemManager().saveFile, {tempFilePath: path}, 'saveFile')
+
+        console.log('newPath', newPath);
+
         const backgroundImage = getStorageSync('backgroundImage', '')
         if (backgroundImage != '') uni.removeStorageSync(backgroundImage)
-        await becomePromise(uni.setStorage, { key: 'backgroundImage', data: newPath }, 'setStorage')
+        await becomePromise(uni.setStorage, {key: 'backgroundImage', data: newPath}, 'setStorage')
         store.commit('common/setBackgroundImage', {
           backgroundImagePath: newPath,
         })
@@ -229,11 +196,13 @@ export default {
 
   .ts-theme-item {
     width: 20%;
+
     .ts-theme-item-show {
       position: relative;
       width: 85rpx;
       height: 85rpx;
       border-radius: 50%;
+
       .t {
         position: absolute;
         top: 50%;
@@ -247,6 +216,7 @@ export default {
     }
   }
 }
+
 .ts-image {
   display: flex;
   flex-direction: row;
